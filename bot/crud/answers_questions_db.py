dcf498de036beecc5c59221c36c83bcab8836a67
Alex Tarasov <at146@ya.ru>
2025-03-17 23:03:54 +0300

    add: Insert если первый вопрос, update в другом случае
    Signed-off-by: Alex Tarasov <at146@ya.ru>

--- bot/crud/answers_questions_db.py
+++ bot/crud/answers_questions_db.py
@@ -1,15 +1,34 @@
-from sqlalchemy import insert
+from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from bot.db.models import AnswerQuestions
 
 
-async def insert_answers_questions(
+async def insert_answer_questions(
+    user_id: int,
+    results: str,
+    db_session: async_sessionmaker[AsyncSession],
+) -> int:
+    async with db_session() as session:
+        answer_questions = AnswerQuestions()
+        answer_questions.user_id = user_id
+        answer_questions.results = results
+        session.add(answer_questions)
+        await session.commit()
+        return answer_questions.id
+
+
+async def update_answer_questions(
+    id_answer_questions: int,
     user_id: int,
     results: str,
     db_session: async_sessionmaker[AsyncSession],
 ) -> None:
     async with db_session() as session:
-        sql = insert(AnswerQuestions).values(user_id=user_id, results=results)
+        sql = (
+            update(AnswerQuestions)
+            .values(results=results)
+            .where(AnswerQuestions.id == id_answer_questions, AnswerQuestions.user_id == user_id)
+        )
         await session.execute(sql)
         await session.commit()
--- bot/dialogs/menu/questioning/callbacks.py
+++ bot/dialogs/menu/questioning/callbacks.py
@@ -4,7 +4,7 @@ from aiogram.types import User
 from aiogram_dialog import ChatEvent, DialogManager
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
-from bot.crud.answers_questions_db import insert_answers_questions
+from bot.crud.answers_questions_db import insert_answer_questions, update_answer_questions
 from bot.dialogs.menu.result_questioning.states import ResultMenu
 
 from .text import DICT_QUESTIONS, LAST_QUESTION
@@ -19,25 +19,33 @@ async def question_clicked(
     if select.widget_id is None:
         raise ValueError("Select Widget ID is None")
 
+    event_from_user: User = manager.middleware_data["event_from_user"]
+    db_session: async_sessionmaker[AsyncSession] = manager.middleware_data["db_session"]
+
     if select.widget_id == "last":
         manager.dialog_data[select.widget_id] = LAST_QUESTION["variants"][int(item_id) - 1]
     else:
         manager.dialog_data[select.widget_id] = DICT_QUESTIONS[select.widget_id]["variants"][int(item_id) - 1]
 
-    # Вопросы кончились
-    if select.widget_id == "last":
-        # await manager.done()
-        event_from_user: User = manager.middleware_data["event_from_user"]
-        db_session: async_sessionmaker[AsyncSession] = manager.middleware_data["db_session"]
-
-        result = {}
-        for key, value in manager.dialog_data.items():
+    result = {}
+    for key, value in manager.dialog_data.items():
+        if key != "id_answer_questions":
             if key == "last":
                 result[LAST_QUESTION["question"]] = value
             else:
                 result[DICT_QUESTIONS[key]["question"]] = value
 
-        await insert_answers_questions(event_from_user.id, str(result), db_session)
+    if select.widget_id == "1":
+        id_answer_questions = await insert_answer_questions(event_from_user.id, str(result), db_session)
+        manager.dialog_data["id_answer_questions"] = id_answer_questions
+    else:
+        await update_answer_questions(
+            manager.dialog_data["id_answer_questions"], event_from_user.id, str(result), db_session
+        )
+
+    # Вопросы кончились
+    if select.widget_id == "last":
+        # await manager.done()
         await manager.start(
             ResultMenu.select_result_menu,
             data={
