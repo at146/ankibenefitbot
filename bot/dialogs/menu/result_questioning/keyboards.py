50ad7c29795583a093133f7f7bacba189ea2bea7
Alex Tarasov <at146@ya.ru>
2025-03-18 02:07:52 +0300

    fix: article_kb
    Signed-off-by: Alex Tarasov <at146@ya.ru>

--- bot/dialogs/menu/result_questioning/keyboards.py
+++ bot/dialogs/menu/result_questioning/keyboards.py
@@ -7,5 +7,4 @@ from bot.core.config import settings
 def article_kb(user_id: int, text: str) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(text=text, url=settings.ARTICLE_REDIRECT_URL.format(user_id=user_id))
-    builder.button(text="Получить статью сейчас", url=settings.ARTICLE_REDIRECT_URL.format(user_id=user_id))
     return builder.as_markup()
