f7b7565adf03fa54299508c125697a2b6cc898d2
Alex Tarasov <at146@ya.ru>
2025-03-19 19:09:23 +0300

    fix: header in worksheet_1

--- bot/api/google_sheets/anki_sheet.py
+++ bot/api/google_sheets/anki_sheet.py
@@ -17,26 +17,31 @@ class AnkiSheet:
         self.gs = gspread.auth.service_account(self.path_credits)
         # подключаем таблицу по ID
         self.table = self.gs.open_by_key(settings.GOOGLE_SHEET_TABLE_ID)
+        self.header_worksheet_1 = self._get_header_worksheet_1()
+        self.header_worksheet_2 = self._get_header_worksheet_2()
 
     async def init_table(self) -> None:
         # TODO: batch_format, format
         await self._init_worksheet_1()
         await self._init_worksheet_2()
 
-    async def _init_worksheet_1(self) -> None:
-        answers_users = await get_results(db_session)
-        result_cell: list[gspread.Cell] = []
-
+    def _get_header_worksheet_1(self):
         header_list = ["Ник ТГ", "Дата и Время входа"]
         for value in DICT_QUESTIONS.values():
             header_list.append(value["question"])  # type: ignore
         header_list.append(LAST_QUESTION["question"])  # type: ignore
+        return header_list
 
-        for col, header in enumerate(header_list, 1):
+    def _get_header_worksheet_2(self):
+        return ["Ник ТГ", "Дата и Время входа", "Забрал статью", "Перешел в канал"]
+
+    async def _init_worksheet_1(self) -> None:
+        answers_users = await get_results(db_session)
+        result_cell: list[gspread.Cell] = []
+
+        for col, header in enumerate(self.header_worksheet_1, 1):
             cell = gspread.Cell(row=1, col=col, value=header)
             result_cell.append(cell)
-
-        answers_users = await get_results(db_session)
 
         # вероятность очень мала))
         random_str = uuid.uuid4().hex
@@ -69,9 +74,8 @@ class AnkiSheet:
     async def _init_worksheet_2(self) -> None:
         users = await get_users(db_session)
         result_cell: list[gspread.Cell] = []
-        header_list = ["Ник ТГ", "Дата и Время входа", "Забрал статью", "Перешел в канал"]
 
-        for col, header in enumerate(header_list, 1):
+        for col, header in enumerate(self.header_worksheet_2, 1):
             cell = gspread.Cell(row=1, col=col, value=header)
             result_cell.append(cell)
 
@@ -98,6 +102,10 @@ class AnkiSheet:
         answers_users = await get_results(db_session)
         row = 2
         result_cell: list[gspread.Cell] = []
+        for col, header in enumerate(self.header_worksheet_1, 1):
+            cell = gspread.Cell(row=1, col=col, value=header)
+            result_cell.append(cell)
+
         for answers_user in answers_users:
             json_acceptable_string = answers_user.results.replace("'", '"')
             dict_value: dict[str, str] = json.loads(json_acceptable_string)
@@ -121,9 +129,7 @@ class AnkiSheet:
     async def _update_worksheet_2(self) -> None:
         users = await get_users(db_session)
         result_cell: list[gspread.Cell] = []
-        header_list = ["Ник ТГ", "Дата и Время входа", "Забрал статью", "Перешел в канал"]
-
-        for col, header in enumerate(header_list, 1):
+        for col, header in enumerate(self.header_worksheet_2, 1):
             cell = gspread.Cell(row=1, col=col, value=header)
             result_cell.append(cell)
 
