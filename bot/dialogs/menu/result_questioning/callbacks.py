be52c56c362561686351c606e4185b053bdeaf6c
Alex Tarasov <at146@ya.ru>
2025-03-20 21:39:39 +0300

    add: delete_message article fix: delete message after send_message
    Signed-off-by: Alex Tarasov <at146@ya.ru>

--- bot/dialogs/menu/result_questioning/callbacks.py
+++ bot/dialogs/menu/result_questioning/callbacks.py
@@ -3,12 +3,13 @@ import datetime
 from aiogram_dialog import DialogManager
 from apscheduler.triggers.date import DateTrigger
 
+from bot.core.config import settings
 from bot.crud import users_db
 from bot.db.session import db_session
 from bot.init import bot, scheduler
 
 from .keyboards import article_kb
-from .text import TEXT_AFTER_23_HOURS, get_after_5_minutes_text
+from .text import LAST_MESSAGE_TEXT, TEXT_AFTER_23_HOURS, get_after_5_minutes_text
 
 
 async def on_start(
@@ -17,6 +18,16 @@ async def on_start(
 ) -> None:
     # TODO: для локальной разработки меньше поставить тригер
     trigger = DateTrigger(run_date=datetime.datetime.now() + datetime.timedelta(minutes=5), timezone="Europe/Moscow")
+
+    if settings.ENVIRONMENT == "production":
+        trigger = DateTrigger(
+            run_date=datetime.datetime.now() + datetime.timedelta(minutes=5), timezone="Europe/Moscow"
+        )
+    else:
+        trigger = DateTrigger(
+            run_date=datetime.datetime.now() + datetime.timedelta(seconds=10), timezone="Europe/Moscow"
+        )
+
     scheduler.add_job(
         change_message_after_5_minutes,
         trigger=trigger,
@@ -36,14 +47,20 @@ async def change_message_after_5_minutes(chat_id: int, message_id: int, first_na
         raise ValueError(f"User not found chat_id - {chat_id}")
 
     if user_db.is_clicked_article is False:
-        await bot.delete_message(chat_id=chat_id, message_id=message_id)
         last_message = await bot.send_message(
             chat_id=chat_id,
             text=get_after_5_minutes_text(),
             reply_markup=article_kb(chat_id, "Читать статью"),
         )
-        # TODO: для локальной разработки меньше поставить тригер
-        trigger = DateTrigger(run_date=datetime.datetime.now() + datetime.timedelta(hours=23), timezone="Europe/Moscow")
+        await bot.delete_message(chat_id=chat_id, message_id=message_id)
+        if settings.ENVIRONMENT == "production":
+            trigger = DateTrigger(
+                run_date=datetime.datetime.now() + datetime.timedelta(hours=23), timezone="Europe/Moscow"
+            )
+        else:
+            trigger = DateTrigger(
+                run_date=datetime.datetime.now() + datetime.timedelta(seconds=10), timezone="Europe/Moscow"
+            )
         scheduler.add_job(
             change_message_after_23_hours,
             trigger=trigger,
@@ -59,9 +76,30 @@ async def change_message_after_23_hours(chat_id: int, message_id: int) -> None:
     if user_db is None:
         raise ValueError(f"User not found chat_id - {chat_id}")
     if user_db.is_clicked_article is False:
-        await bot.delete_message(chat_id=chat_id, message_id=message_id)
-        await bot.send_message(
+        last_message = await bot.send_message(
             chat_id=chat_id,
             text=TEXT_AFTER_23_HOURS,
             reply_markup=article_kb(chat_id, "Забрать статью"),
         )
+        await bot.delete_message(chat_id=chat_id, message_id=message_id)
+        if settings.ENVIRONMENT == "production":
+            trigger = DateTrigger(
+                run_date=datetime.datetime.now() + datetime.timedelta(hours=1), timezone="Europe/Moscow"
+            )
+        else:
+            trigger = DateTrigger(
+                run_date=datetime.datetime.now() + datetime.timedelta(seconds=10), timezone="Europe/Moscow"
+            )
+        scheduler.add_job(
+            delete_message,
+            trigger=trigger,
+            args=[
+                last_message.chat.id,
+                last_message.message_id,
+            ],
+        )
+
+
+async def delete_message(chat_id: int, message_id: int) -> None:
+    await bot.send_message(chat_id=chat_id, text=LAST_MESSAGE_TEXT)
+    await bot.delete_message(chat_id=chat_id, message_id=message_id)
