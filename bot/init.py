b60b6f18086296169456525dfbccb6c6e3841c5e
Alex Tarasov <at146@ya.ru>
2025-07-08 21:56:11 +0300

    ci: add dependabot

--- bot/init.py
+++ bot/init.py
@@ -3,10 +3,10 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.client.session.aiohttp import AiohttpSession
 
 from bot.core.config import settings
-from bot.utils import logging
+from bot.utils.logging import setup_logger
 from bot.utils.scheduler import setup_scheduler
 
-log = logging.setup_logger()
+log = setup_logger()
 session = AiohttpSession()
 bot = Bot(
     token=settings.BOT_TOKEN,
